9fdbc619ed01d40258e25bfd67ee956732641603
LawrentySeverin <140348036+LawrentySeverin@users.noreply.github.com>
2023-09-27 00:38:29 +0300

    add pipe
    добавил 2 связанные функции, отвечающие соответственно за ввод и вывод информации о трубе

--- Severin_lab_1/Severin_lab_1/Severin_lab_1.cpp
+++ Severin_lab_1/Severin_lab_1/Severin_lab_1.cpp
@@ -4,17 +4,11 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-
-    
-}
-
 
 struct pipe // создание новой кастомной переменной "труба"
 {
     string title;
-    float lenght;
+    float length;
     float diameter;
     bool mark;
 };
@@ -25,8 +19,41 @@ struct CS
     string efficiency;
     int workshops;
     int workshops_at_work;
-    
 };
+
+pipe inputPipe()
+{
+    pipe p;
+    cout << "type title of pipe: ";
+    cin >> p.title;
+    cout << "type lenhgth of pipe: ";
+    cin >> p.length;
+    cout << "type diameter of pipe: ";
+    cin >> p.diameter;
+    cout << "type mark of pipe (in repair or not): ";
+    cin >> p.mark;
+    return p;
+}
+
+
+void outputPipe(pipe p)
+{
+    cout << "title of pipe: " << p.title << endl
+        << "length of pipe: " << p.length << endl
+        << "diameter of pipe: " << p.diameter << endl
+        << "mark of pipe (in repair or not): " << p.mark << endl;
+}
+
+int main()
+{
+    
+    //inputPipe();
+    pipe pip = inputPipe();
+    outputPipe(pip);
+
+
+
+}
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
